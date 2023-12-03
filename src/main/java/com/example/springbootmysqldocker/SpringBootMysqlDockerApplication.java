d6606bebb7eebce8d309fbd23358efcf308b7e73
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2023-12-03 19:09:17 +0530

    considerations.

--- src/main/java/com/example/springbootmysqldocker/SpringBootMysqlDockerApplication.java
+++ src/main/java/com/example/springbootmysqldocker/SpringBootMysqlDockerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMysqlDockerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMysqlDockerApplication.class, args);
 	}
